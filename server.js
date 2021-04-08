1cbe7e5a94b8c5c0a493cb1eff92d7eb7b1ca3e6
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-08 13:47:29 +0200

    feat: add cloned frontend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-    res.json({message: "Ca marche"});
+    res.json({message: "Ca marche toujours"});
 });
 
 module.exports = app;
