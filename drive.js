a35215e75e290055c3abd2aef7fe0d59f01c9489
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-20 14:58:34 +0200

    feat: list all files

--- drive.js
+++ drive.js
@@ -1,44 +1,53 @@
-const path = require("path")
-const fs = require("fs/promises")
-const os = require("os")
-const ALPS_DRIVE_ROOT = path.join(os.tmpdir(), "alpsDrive")
-//Path tmp
-console.log("truc", ALPS_DRIVE_ROOT)
+const path = require('path')
+const fs = require('fs/promises')
+const os = require('os')
+const ALPS_DRIVE_ROOT = path.join(os.tmpdir(), 'alpsDrive')
 
 function logFolderExist() {
     console.log("Folder exist");
 }
 
+// Create a folder alpsDrive
 function createRootFolderNoVerify() {
     return fs.mkdir(ALPS_DRIVE_ROOT);
 }
 
+// Check if foler exist
 function rootFolderExists() {
     return fs.access(ALPS_DRIVE_ROOT);
 }
 
+// Create a folder or not if already exist
 function createRootFolder() {
     return rootFolderExists()
         .then(logFolderExist)
         .catch(createRootFolderNoVerify);
 }
 
+// List all the files and folder
 function listAll() {
-    // directory path
-    const directoryPath = path.join(__dirname, "files")
-
-    fs.readdir(directoryPath, function (err, files) {
-        if (err) {
-            console.log("Error getting directory information.")
-        } else {
-            files.forEach(function (file) {
-                console.log(file)
+    const allFoldersAndFilesPromise = fs.readdir(ALPS_DRIVE_ROOT, {withFileTypes:true})
+    return allFoldersAndFilesPromise.then((folderAndFilesList) => {
+        //console.log(folderAndFilesList)
+        const folderAndFilesTab = []
+        folderAndFilesList.forEach((result) => {
+            folderAndFilesTab.push({
+                name: result.name,
+                isFolder: result.isDirectory(),
             })
-        }
-    });
+        })
+        //console.log(folderAndFilesTab)
+        return folderAndFilesTab
+    })
+}
+
+// Open a selected file or folder
+function openFileOrFolder() {
+
 }
 
 module.exports = {
     createRootFolder: createRootFolder,
     listAll: listAll,
+    openFileOrFolder: openFileOrFolder,
 };
--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const app = express();
 const drive = require('./drive');
 
 function start() {
+    app.use(express.static('frontend'));
     app.listen(3000, () => {
     })
 }
 
-app.use(express.static('frontend'));
-
-
-app.use((req, res) => {
-    res.json({message: "Ca marche toujours"});
+app.get("/api/drive", (req, res) => {
+    drive.listAll().then(list => {
+        res.send(list);
+    })
 });
 
-app.get("/api/drive", (req, res) => {
-    res.send(drive.listAll);
+app.get("/api/drive/{name}", (req, res) => {
+   drive.openFileOrFolder(name).then(result => {
+       res.send(result)
+   })
 });
 
 module.exports = {
